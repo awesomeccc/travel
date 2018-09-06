f3c8610b17246838b0223e12da5969f202e2f5e0
旧梦丶 <329694993@qq.com>
2018-09-06 14:18:12 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body) // 解决移动端300ms延迟
+
+new Vue({
+  router,
+  components: { App },
+  template: '<App/>',
+  render: h => h(App)
+}).$mount('#app')
+
+// import Vue from 'vue'
+// import App from './App.vue'
+// import router from './router'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App)
+// }).$mount('#app')
+
+// new Vue({
+//   el: '#app',
+//   router,
+//   components:{ App },
+//  })
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <div class="home">
+    home
+  </div>
+  <!-- <router-link to='list'>list</router-link> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+//export default {
+ // name: 'home',
+  //components: {
+   // HelloWorld
+//  }
+//}
+</script>
+
+<style></style>
